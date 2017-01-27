3c1a506ccd38dc87740d096f9f2119914cacf996
John H. Robinson, IV <jhriv@ucsd.edu>
2017-01-26 17:17:17 -0800

    Move nav links inside "primary" division

--- items/show.php
+++ items/show.php
@@ -2,12 +2,12 @@
 
 <h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
 
+<div id="primary">
+
 <ul class="item-pagination navigation">
     <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
     <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
 </ul>
-
-<div id="primary">
 
     <?php if ((get_theme_option('Item FileGallery') == 0) && metadata('item', 'has files')): ?>
     <?php echo files_for_item(array('imageSize' => 'fullsize')); ?>
