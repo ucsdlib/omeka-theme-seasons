d42174f5d54a2c0e7f57b51158f31d935c48da7b
John H. Robinson, IV <jhriv@ucsd.edu>
2017-01-26 17:22:38 -0800

    Move nav links above H1

--- items/show.php
+++ items/show.php
@@ -1,13 +1,13 @@
 <?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'items show')); ?>
 
-<h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
-
-<div id="primary">
-
 <ul class="item-pagination navigation">
     <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
     <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
 </ul>
+
+<h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
+
+<div id="primary">
 
     <?php if ((get_theme_option('Item FileGallery') == 0) && metadata('item', 'has files')): ?>
     <?php echo files_for_item(array('imageSize' => 'fullsize')); ?>
