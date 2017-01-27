115fe2eba75fc2c094ddd821481ebc630a97c2f1
John H. Robinson, IV <jhriv@ucsd.edu>
2017-01-26 17:11:55 -0800

    Move navigation links to top

--- items/show.php
+++ items/show.php
@@ -2,6 +2,11 @@
 
 <h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
 
+<ul class="item-pagination navigation">
+    <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+    <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+</ul>
+
 <div id="primary">
 
     <?php if ((get_theme_option('Item FileGallery') == 0) && metadata('item', 'has files')): ?>
@@ -47,10 +52,5 @@
     </div>
 
 </aside>
-
-<ul class="item-pagination navigation">
-    <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-    <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-</ul>
 
 <?php echo foot(); ?>
